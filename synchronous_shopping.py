e657324fb5d3bda4ad6fdc9aff29d8b166095b3f
Aaron Robson <5177808+AaronRobson@users.noreply.github.com>
2023-05-23 23:05:07 +0100

    handle cases where the start and end vertexes have all the fishes we need

--- synchronous_shopping.py
+++ synchronous_shopping.py
@@ -1,52 +1,128 @@
-#!/bin/python3
+# https://www.hackerrank.com/challenges/synchronous-shopping/problem
 
-# import math
-import os
-# import random
-# import re
-# import sys
+from typing import Dict, Optional, Tuple, Set, NamedTuple
+from sys import maxsize
 
-#
-# Complete the 'shop' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER k
-#  3. STRING_ARRAY centers
-#  4. 2D_INTEGER_ARRAY roads
-#
+Cache = Dict[Set[int], int]
 
 
 def shop(n, k, centers, roads):
-    # Write your code here
+    # cats_count = 2
+    # cats = parse_cats(cats_count)
+
+    vertices = parse_vertices(n)
+    starting_vertex = vertices[0]
+    finishing_vertex = vertices[-1]
+
+    fishes = parse_fishes(k)
+
+    centers = parse_centers(centers)
+    roads = parse_roads(roads)
+
+    cache: Cache = {}
+
+    cache.update(
+        {
+            frozenset({starting_vertex, a}): cost
+            for a, cost in dijkstra(
+                vertices=vertices,
+                edges=roads,
+                from_=starting_vertex,
+            ).items()
+        }
+    )
+
+    fishes = set(fishes) - centers[starting_vertex] - centers[finishing_vertex]
+    if not fishes:
+        return cache[frozenset({starting_vertex, finishing_vertex})]
+
+    # todo have the cats explore different combinations of other centers for fishes we need
     return 0
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+def _one_to_n(n: int) -> Tuple[int, ...]:
+    return tuple(range(1, n + 1))
 
-    first_multiple_input = input().rstrip().split()
 
-    n = int(first_multiple_input[0])
+parse_cats = _one_to_n
+parse_vertices = _one_to_n
+parse_fishes = _one_to_n
 
-    m = int(first_multiple_input[1])
 
-    k = int(first_multiple_input[2])
+def parse_centers(centers):
+    return {
+        i: set(map(int, center.split()[1:]))
+        for i, center in enumerate(centers, start=1)
+    }
 
-    centers = []
 
-    for _ in range(n):
-        centers_item = input()
-        centers.append(centers_item)
+class Road(NamedTuple):
+    route: Set[int]  # Expected to have a length of exactly 2.
+    cost: int  # Expected to not be negative.
 
-    roads = []
 
-    for _ in range(m):
-        roads.append(list(map(int, input().rstrip().split())))
+Route = Road
 
-    res = shop(n, k, centers, roads)
+# class RoadFrom(NamedTuple):
+#     to_: int
+#     cost: int  # Expected to not be negative.
 
-    fptr.write(str(res) + '\n')
 
-    fptr.close()
+def parse_roads(roads) -> Tuple[Road, ...]:
+    return tuple(
+        Road(route={road[0], road[1]}, cost=road[2])
+        for road in roads)
+
+
+def dijkstra(vertices: Tuple[int], edges: Tuple[Road], from_: int) -> Dict[int, int]:
+    '''https://www.youtube.com/watch?v=EFg3u_E6eHU
+    '''
+    set_latest = {vertex: Node() for vertex in vertices}
+    set_latest[from_].latest_cost = 0
+    node_in_progress: Optional[int] = from_
+
+    while node_in_progress is not None:
+        for to, cost in _find_direct_roads(edges=edges, from_=node_in_progress).items():
+            if set_latest[to].explored:
+                continue
+            this_cost = set_latest[node_in_progress].latest_cost + cost
+            if set_latest[to].latest_cost > this_cost:
+                set_latest[to].latest_cost = this_cost
+
+        set_latest[node_in_progress].explored = True
+        node_in_progress = _find_new_node_in_progress(set_latest)
+
+    return {
+        key: value.latest_cost
+        for key, value in set_latest.items()
+    }
+
+
+class Node():
+    def __init__(self, *, latest_cost: int = maxsize, explored: bool = False):
+        self.latest_cost = latest_cost
+        self.explored = explored
+
+    def __repr__(self):
+        items = []
+        if self.latest_cost != maxsize:
+            items.append(f'latest_cost={self.latest_cost!r}')
+        if self.explored:
+            items.append(f'explored={self.explored!r}')
+        return f'{self.__class__.__name__}({", ".join(items)})'
+
+
+def _find_direct_roads(*, edges: Tuple[Road], from_: int) -> Dict[int, int]:
+    output = {}
+    for edge in edges:
+        if from_ in edge.route:
+            to_ = list(edge.route - {from_})[0]
+            output[to_] = edge.cost
+    return output
+
+
+def _find_new_node_in_progress(set_latest) -> Optional[int]:
+    unexplored = [(key, value) for key, value in set_latest.items() if not value.explored]
+    if not unexplored:
+        return None
+    return min(unexplored, key=lambda item: item[1].latest_cost)[0]
--- test_synchronous_shopping.py
+++ test_synchronous_shopping.py
@@ -1,7 +1,16 @@
 import unittest
+from sys import maxsize
 
 from synchronous_shopping import (
     shop,
+    parse_vertices,
+    parse_centers,
+    Road,
+    parse_roads,
+    Node,
+    _find_direct_roads,
+    _find_new_node_in_progress,
+    dijkstra,
 )
 
 
@@ -29,7 +38,6 @@ class TestShop(unittest.TestCase):
             ),
             31)
 
-    @unittest.expectedFailure
     def test_sample_test_case_1(self):
         self.assertEqual(
             shop(
@@ -59,5 +67,185 @@ class TestShop(unittest.TestCase):
             792)
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestParseVertices(unittest.TestCase):
+    def test(self):
+        self.assertEqual(
+            parse_vertices(5),
+            (1, 2, 3, 4, 5),
+        )
+
+
+class TestParseCenters(unittest.TestCase):
+    def test_sample_test_case_0(self):
+        self.assertEqual(
+            parse_centers([
+                '1 1',
+                '1 2',
+                '1 3',
+                '1 4',
+                '1 5',
+            ]),
+            {
+                1: {1},
+                2: {2},
+                3: {3},
+                4: {4},
+                5: {5},
+            })
+
+    def test_sample_test_case_1(self):
+        self.assertEqual(
+            parse_centers([
+                '2 1 2',
+                '1 3',
+                '0',
+                '2 1 3',
+                '1 2',
+                '1 3'
+            ]),
+            {
+                1: {1, 2},
+                2: {3},
+                3: set(),
+                4: {1, 3},
+                5: {2},
+                6: {3},
+            })
+
+
+class TestParseRoads(unittest.TestCase):
+    def test_sample_test_case_0(self):
+        self.assertEqual(
+            parse_roads([
+                [1, 2, 10],
+                [1, 3, 10],
+                [2, 4, 10],
+                [3, 5, 10],
+                [4, 5, 10],
+            ]),
+            (
+                Road(route={1, 2}, cost=10),
+                Road(route={1, 3}, cost=10),
+                Road(route={2, 4}, cost=10),
+                Road(route={3, 5}, cost=10),
+                Road(route={4, 5}, cost=10),
+            ),
+        )
+
+    def test_sample_test_case_1(self):
+        self.maxDiff = None
+        self.assertEqual(
+            parse_roads([
+                [1, 2, 572],
+                [4, 2, 913],
+                [2, 6, 220],
+                [1, 3, 579],
+                [2, 3, 808],
+                [5, 3, 298],
+                [6, 1, 927],
+                [4, 5, 171],
+                [1, 5, 671],
+                [2, 5, 463],
+            ]),
+            (
+                Road(route={1, 2}, cost=572),
+                Road(route={4, 2}, cost=913),
+                Road(route={2, 6}, cost=220),
+                Road(route={1, 3}, cost=579),
+                Road(route={2, 3}, cost=808),
+                Road(route={5, 3}, cost=298),
+                Road(route={6, 1}, cost=927),
+                Road(route={4, 5}, cost=171),
+                Road(route={1, 5}, cost=671),
+                Road(route={2, 5}, cost=463),
+            ))
+
+
+class TestDijkstra(unittest.TestCase):
+    def test_sample_test_case_0(self):
+        self.assertEqual(
+            dijkstra(
+                vertices=(1, 2, 3, 4, 5),
+                edges=(
+                    Road(route={1, 2}, cost=10),
+                    Road(route={1, 3}, cost=10),
+                    Road(route={2, 4}, cost=10),
+                    Road(route={3, 5}, cost=10),
+                    Road(route={4, 5}, cost=10),
+                ),
+                from_=1),
+            {
+                1: 0,
+                2: 10,
+                3: 10,
+                4: 20,
+                5: 20,
+            })
+
+
+class TestNode(unittest.TestCase):
+    def test(self):
+        a = Node()
+        self.assertEqual(a.latest_cost, maxsize)
+        self.assertEqual(a.explored, False)
+        a.latest_cost = 5
+        a.explored = True
+        self.assertEqual(a.latest_cost, 5)
+        self.assertEqual(a.explored, True)
+
+
+class TestFindDirectRoads(unittest.TestCase):
+    def test_sample_test_case_0(self):
+        self.assertEqual(
+            _find_direct_roads(
+                edges=(
+                    Road(route={1, 2}, cost=10),
+                    Road(route={1, 3}, cost=10),
+                    Road(route={2, 4}, cost=10),
+                    Road(route={3, 5}, cost=10),
+                    Road(route={4, 5}, cost=10),
+                ),
+                from_=1,
+            ),
+            {
+                2: 10,
+                3: 10,
+            })
+
+
+class TestFindNewNodeInProgress(unittest.TestCase):
+    def test_initial(self):
+        self.assertEqual(
+            _find_new_node_in_progress(
+                {
+                    1: Node(latest_cost=0),
+                    2: Node(),
+                    3: Node(),
+                }
+            ),
+            1,
+        )
+
+    def test_one_possible(self):
+        self.assertEqual(
+            _find_new_node_in_progress(
+                {
+                    1: Node(latest_cost=0, explored=True),
+                    2: Node(latest_cost=10),
+                    3: Node(),
+                }
+            ),
+            2,
+        )
+
+    def test_two_possible(self):
+        self.assertIn(
+            _find_new_node_in_progress(
+                {
+                    1: Node(latest_cost=0, explored=True),
+                    2: Node(latest_cost=10),
+                    3: Node(latest_cost=10),
+                }
+            ),
+            [2, 3],
+        )
