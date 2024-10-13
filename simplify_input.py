82e0a58406aaf19af064225b49ed735f60539d57
Aaron Robson <5177808+AaronRobson@users.noreply.github.com>
2024-10-13 04:50:44 +0100

    simplify chains in center/road graphs
    For instance:
    1. 'cat input05.txt | python3 simplify_input.py | python3 synchronous_shopping.py'
       Results in the start and end centers connected by a single road.
    2. 'cat input21.txt | python3 simplify_input.py | python3 synchronous_shopping.py'
       Can run in a more sensible time frame than before.

--- simplify_input.py
+++ simplify_input.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 def input_data() -> dict:
     first_multiple_input = input().rstrip().split()
 
@@ -130,10 +133,11 @@ def simplify_deadends(
             center_links[to_].add(from_)
         centers_to_remove = set()
         for center, links in center_links.items():
-            # skip removing the start or fish centers
+            # skip removing the start or end centers
             if center in (1, center_count):
                 continue
             # skip removing center with fish
+            # TODO consider allowing centers with only fish that the neighbour already has to be removed
             if centers[center - 1] != '0':
                 continue
             # skip removing centers that aren't dead ends
@@ -174,10 +178,90 @@ def simplify_deadends(
     }
 
 
+def simplify_chains(
+    *,
+    center_count,
+    road_count,
+    fish_count,
+    centers,
+    roads,
+):
+    while True:
+        center_links = {i: {} for i in range(1, center_count + 1)}
+        for road in roads:
+            from_, to_, cost = map(int, road)
+            center_links[from_][to_] = cost
+            center_links[to_][from_] = cost
+        center_to_remove: Optional[int] = None
+        for center, links in center_links.items():
+            # skip removing the start or end centers
+            if center in (1, center_count):
+                continue
+            # skip removing center with fish
+            # TODO consider allowing centers with only fish that both neighbours already have to be removed
+            if centers[center - 1] != '0':
+                continue
+            # skip centers which don't have exactly two roads
+            if len(links) != 2:
+                continue
+            center_to_remove = center
+            break
+        if center_to_remove is None:
+            break
+        centers = [center for i, center in enumerate(centers, start=1) if i != center_to_remove]
+        center_count = len(centers)
+        # remove center
+        roads = [
+            [from_, to_, cost]
+            for from_, to_, cost in roads
+            if (from_ != center_to_remove) and (to_ != center_to_remove)
+        ]
+        # see if a bypass is worthwhile
+        from_, to_ = list(links.keys())
+        bypass_length = sum(links.values())
+        try:
+            direct_length = center_links[from_][to_]
+        except KeyError:
+            direct_length = None
+        if direct_length is None:
+            # include the bypass road
+            roads.append([from_, to_, bypass_length])
+        elif direct_length < bypass_length:
+            pass  # direct is already quicker
+        else:
+            # replace direct with quicker bypass
+            roads = [
+                [from_, to_, cost]
+                for from_, to_, cost in roads
+                if set([from_, to_]) != set(links.keys())
+            ]
+            roads.append([from_, to_, bypass_length])
+        # shift down road references to centers
+        roads = [
+            [
+                from_ if from_ < center_to_remove else (from_ - 1),
+                to_ if to_ < center_to_remove else (to_ - 1),
+                cost,
+            ]
+            for from_, to_, cost in roads
+        ]
+        road_count = len(roads)
+        # break
+
+    return {
+        'center_count': center_count,
+        'road_count': road_count,
+        'fish_count': fish_count,
+        'centers': centers,
+        'roads': roads,
+    }
+
+
 config = [
     simplify_id,
     simplify_fish,
     simplify_deadends,
+    simplify_chains,
 ]
 
 
